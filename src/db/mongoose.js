32ddd78c78dd9742b9b49235832ab39261e053be
jameshughes7 <hugjimbo77@gmail.com>
2019-05-03 11:38:33 +0100

    npm validator added and validation used with Users collection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true, 
     useCreateIndex: true
@@ -7,17 +9,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // Defining model
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error ('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            } 
+        }
     }
 })
 
 // Creating instance of model
 const me = new User({
-    name: 'James',
-    age: 36
+    name: "   James   Andrew     Hughes",
+    email: '   HUGJIMBO77@GMAIL.COM   '
 })
 
 // Saving instance of model instance to the db
@@ -26,3 +47,27 @@ me.save().then(() => {
 }).catch ((error) => {
     console.log(error)
 })
+
+// Define tasks model
+// pass in name and then define the model
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+// // Create new instance of model
+// const dishes = new Task({
+//     description: 'Dishes',
+//     boolean: false
+// })
+
+// // Save model to the database
+// dishes.save().then(() => {
+//     console.log(dishes)
+// }).catch ((error) => {
+//     console.log(error)
+// })
