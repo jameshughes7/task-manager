1a59d438f498daef0554a94fa81e8ccffb9a52a4
jameshughes7 <hugjimbo77@gmail.com>
2019-05-03 13:59:21 +0100

    refactored Task model and added POST endpoint for creating tasks

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-// Defining model
+// Defining the User model
 const User = mongoose.model('User', {
     name: {
         type: String,
