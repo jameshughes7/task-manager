9596a1443a9c686e0ef3524e7f82bc5800d6e8f3
jameshughes7 <hugjimbo77@gmail.com>
2019-05-08 16:00:31 +0100

    formatting of avatar image to smaller size and to png

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const sharp = require('sharp');
 
 const router = new express.Router();
 
@@ -125,7 +126,14 @@ const upload = multer({
 
 // Uploading avatar profile
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+        .resize({
+            width: 250,
+            height: 250
+        })
+        .png()
+        .toBuffer()
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
@@ -148,7 +156,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type','image/jpg');
+        res.set('Content-Type','image/png');
         res.send(user.avatar);
     } catch (error) {
         res.status(404).send();
